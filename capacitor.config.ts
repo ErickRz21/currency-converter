df283d8fbfbe00b3d626d07cd16d26507e4a0e2f
ErickRz21 <erickuriel238@gmail.com>
2026-08-12 17:46:00 -0600

    Updated npm packages and installed capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.currency.app',
+  appName: 'Currency Converter',
+  webDir: 'www'
+};
+
+export default config;
